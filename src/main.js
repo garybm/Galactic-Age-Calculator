cb5a33a7d7bafa5952f637458e3463ecbc3644a7
Gary Bermudez <garybm.17@gmail.com>
2019-03-15 10:31:26 -0700

    update babel

--- src/main.js
+++ src/main.js
@@ -5,20 +5,25 @@
   import './sass/styles.scss';
 
 
-   class Birthday {
-    constructor(birthday){
-      this.month = month;
-      this.day = day;
-      this.year = year;
-      console.log(this.month, this.day, this.year);
-    }
-  }
-    const birthday = new Birthday (28, 1, 1984);
 
-
-   function calculateAge(){
+    calculateAge(){
     const currentDate= new Date();
     const today_year = currentDate.getFullYear();
     const today_month = currentDate.getMonth();
     const today_day = currentDate.getDate();
-    // const birthday_year = this.birthday;
+
+
+
+    $(document).ready( function() {
+      $(".date").submit(function(event) {
+        event.preventDefault();
+        const month = $('#month').val();
+        const day = $('#day').val();
+        const year = $('#year').val();
+        const birthday = month.concat(" ", day, ", ", year);
+        const output = calculateAge(newDate);
+        $(".output").text(output);
+
+      })
+
+    })
