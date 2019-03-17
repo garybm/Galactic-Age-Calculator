d2edb27b36267aa04b1869b27c066f95ef1afd23
Gary Bermudez <garybm.17@gmail.com>
2019-03-17 16:28:38 -0700

    fix function to return Venus age, separate bussines from UI

--- src/age.js
+++ src/age.js
@@ -1,8 +1,7 @@
 
 
 //Function to calculate the AGE
-
-function getAge(dateString) {
+export function getAge(dateString) {
     const today = new Date();
     const birthDate = new Date(dateString);
     let age = today.getFullYear() -birthDate.getFullYear();
@@ -14,34 +13,27 @@ function getAge(dateString) {
     return age;
   }
 
-
   //Mercury Age
-
-  function mecuryAge(dateString) {
-  const mercuryAge = getAge(dateString) / .24;
-  return Math.round(mercuryAge);
+  export function mercuryAge(dateString) {
+    const mercuryAge = getAge(dateString) / .24;
+    return Math.round(mercuryAge);
 }
 
   //Venus
-  function venusAge(dateString) {
-  const venusAge = getAge(dateString) / .62;
-  Math.round(venusAge);
-  console.log(venusAge);
-}
+  export function venusAge(dateString) {
+    const venusAge = getAge(dateString) / .62;
+    return Math.round(venusAge);
+ }
 
   //Mars
 
-  function marsAge(dateString) {
-  const marsAge = getAge(dateString) / 1.88;
-  return Math.round(marsAge);
+  export function marsAge(dateString) {
+    const marsAge = getAge(dateString) / 1.88;
+    return Math.round(marsAge);
   }
 
   //Jupiter
-  function jupiterAge(dateString) {
-  const jupiterAge = getAge(dateString) /11.86;
-  return Math.round(jupiterAge);
+  export function jupiterAge(dateString) {
+    const jupiterAge = getAge(dateString) /11.86;
+    return Math.round(jupiterAge);
   }
-
-
-
-  console.log('age: ' + getAge("1984/06/17"));
--- src/main.js
+++ src/main.js
@@ -1,44 +1,10 @@
   import $ from 'jquery';
-  // import { getAge } from './../src/age.js';
+  import { getAge} from './../src/age.js';
+  import { mercuryAge } from './../src/age.js';
+  import { venusAge } from './../src/age.js';
+  import { marsAge } from './../src/age.js';
+  import { jupiterAge } from './../src/age.js';
 
-
-  function getAge(dateString) {
-      const today = new Date();
-      const birthDate = new Date(dateString);
-      let age = today.getFullYear() - birthDate.getFullYear();
-      const month = today.getMonth() - birthDate.getMonth();
-      if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate()))
-      {
-          age--;
-      }
-      return age;
-    }
-
-    //Mercury
-    function mecuryAge(dateString) {
-    const mercuryAge = getAge(dateString) / .24;
-    return Math.round(mercuryAge);
-  }
-
-
-  //Jupiter
-  function jupiterAge(dateString) {
-    const jupiterAge = getAge(dateString) / 11.86;
-    return Math.round(jupiterAge);
-  }
-
-    //Mars
-
-    function marsAge(dateString) {
-    const marsAge = getAge(dateString) / 1.88;
-    return Math.round(marsAge);
-    }
-
-    //Venus
-    function venusAge(dateString) {
-      const venusAge = getAge(dateString) / .62;
-      Math.round(venusAge);
-    }
 
   $(document).ready( function() {
     $(".date").submit(function(event) {
@@ -52,7 +18,7 @@
       $("#jupiter").append(jupiter);
       const venus = venusAge(newDate);
       $("#venus").append(venus);
-      const mercury = mecuryAge(newDate);
+      const mercury = mercuryAge(newDate);
       $("#mercury").append(mercury);
     });
 });
