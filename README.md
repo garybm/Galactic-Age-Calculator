bb7f47818d382188e836cc413255f247168a7e56
Gary Bermudez <garybm.17@gmail.com>
2019-03-22 12:52:03 -0700

    add details to README

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 #  Galactic Age Calculator
 
-_Published_  2/15/2019 <br>
+_Published_  3/15/2019 <br>
 _Author_ **Gary Bermudez**
 
 ### Description
 Javascript objects allowing a User to input a birthday and find out life expectancy and age on different planets.
-Used to practice testing with Jasmine and Karma
+Used to practice testing with Jasmine and Karma.
 
 ### Installation
 * Clone from https://github.com/garybm/independent-project1
@@ -21,11 +21,23 @@ Can Determine Age on Mercury based on that age | 34 | 142
 Can Determine Age on Venus based on that age | 34 | 55
 Can Determine Age on Mars based on that age | 34 | 18
 Can Determine Age on Jupiter based on that age | 34 | 3
-Can give the user life expectancy based on 78 years 
+Can give the user life expectancy based on 78 years | 34 | 44
 
 ### Known Bugs
 * None identified
 
+### Support
+* If you have problems or would like to comment about something feel free to contact me at garybm.17@gmail.com.
+
+
 ### Technologies Used
+* JavaScript
+* HTML
 * Webpack
 * Tested with Karma and Jasmine
+
+### License
+
+*{This website is licensed under the MIT license.*
+
+Copyright (c) 2016 **_Gary Bermudez_**
--- spec/age-spec.js
+++ spec/age-spec.js
@@ -5,29 +5,29 @@ import { marsAge } from './../src/age.js';
 import { jupiterAge } from './../src/age.js';
 import { liveLong } from './../src/age.js';
 
-describe ('getAge', function(){
+describe ('Age', function(){
 
-  it ("calculate the users age", function(){
+  it ("calculates the users age", function(){
     expect(getAge("6/17/1984")).toEqual(34);
   });
 
-  it ("calculate the user age in Mercury", function(){
+  it ("calculates the user age in Mercury", function(){
     expect(mercuryAge("6/17/1984")).toEqual(142);
   });
 
-  it ("calculate the user age in Venus", function(){
+  it ("calculates the user age in Venus", function(){
     expect(venusAge("6/17/1984")).toEqual(55);
   });
 
-  it ("calculate the user afe in Mars", function() {
+  it ("calculates the user afe in Mars", function() {
     expect(marsAge("6/17/1984")).toEqual(18);
   });
 
-  it ("calculate the age in Jupiter", function() {
+  it ("calculates the age in Jupiter", function() {
     expect(jupiterAge("6/17/1984")).toEqual(3);
   })
 
-  it ("calculate the user life expectancy", function() {
+  it ("calculates the user life expectancy", function() {
     expect(liveLong("6/17/1984")).toEqual('You are expected to live for 44 more years!');
   });
 
